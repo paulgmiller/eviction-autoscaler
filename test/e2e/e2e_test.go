09a70e58e59d0bb2a5e589303dea2fc5761c5f4d
paul miller <paul.miller>
2026-05-19 21:32:42 -0700

    fixed new lint errors

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -104,6 +104,8 @@ var _ = Describe("controller", Ordered, func() {
 		}
 	})
 
+	const helmChart = "helm/eviction-autoscaler"
+
 	Context("Operator", func() {
 		ctx := context.Background()
 
@@ -163,7 +165,7 @@ var _ = Describe("controller", Ordered, func() {
 			// the image tag doesn't exist in any remote registry
 			// if pullPolicy=Always would fail trying to pull from remote registry
 			helmArgs := []string{
-				"upgrade", "--install", "eviction-autoscaler", "helm/eviction-autoscaler",
+				"upgrade", "--install", "eviction-autoscaler", helmChart,
 				"--namespace", namespace, "--create-namespace",
 				"--set", fmt.Sprintf("image.repository=%s", repo),
 				"--set", fmt.Sprintf("image.tag=%s", tag),
@@ -685,7 +687,7 @@ var _ = Describe("controller", Ordered, func() {
 			tag := imgParts[1]
 
 			helmArgs := []string{
-				"upgrade", "--install", "eviction-autoscaler", "helm/eviction-autoscaler",
+				"upgrade", "--install", "eviction-autoscaler", helmChart,
 				"--namespace", namespace, "--create-namespace",
 				"--set", fmt.Sprintf("image.repository=%s", repo),
 				"--set", fmt.Sprintf("image.tag=%s", tag),
@@ -781,7 +783,7 @@ var _ = Describe("controller", Ordered, func() {
 			time.Sleep(10 * time.Second)
 
 			helmArgsOptIn := []string{
-				"upgrade", "--install", "eviction-autoscaler", "helm/eviction-autoscaler",
+				"upgrade", "--install", "eviction-autoscaler", helmChart,
 				"--namespace", namespace, "--create-namespace",
 				"--set", fmt.Sprintf("image.repository=%s", repo),
 				"--set", fmt.Sprintf("image.tag=%s", tag),
@@ -791,12 +793,12 @@ var _ = Describe("controller", Ordered, func() {
 				"--set", "controllerConfig.namespaces.actionedNamespaces[0]=kube-system",
 				"--set", "controllerConfig.namespaces.actionedNamespaces[1]=actioned-test",
 			}
-			cmd = exec.Command("helm", helmArgsOptIn...)
+			cmd = exec.CommandContext(ctx, "helm", helmArgsOptIn...)
 			_, err = utils.Run(cmd)
 			ExpectWithOffset(1, err).NotTo(HaveOccurred())
 
 			By("waiting for deployment to be ready")
-			cmd = exec.Command("kubectl", "wait", "--for=condition=available",
+			cmd = exec.CommandContext(ctx, "kubectl", "wait", "--for=condition=available",
 				"deployment/eviction-autoscaler",
 				"--namespace", namespace, "--timeout=300s")
 			_, err = utils.Run(cmd)
@@ -805,7 +807,7 @@ var _ = Describe("controller", Ordered, func() {
 			// Test 5: enabled_by_default=false - namespace without annotation should be disabled
 			testNsOptIn := "test-opt-in-mode"
 			By("creating a namespace without annotation (should be disabled in opt-in mode)")
-			cmd = exec.Command("kubectl", "create", "namespace", testNsOptIn)
+			cmd = exec.CommandContext(ctx, "kubectl", "create", "namespace", testNsOptIn)
 			_, err = utils.Run(cmd)
 			Expect(err).NotTo(HaveOccurred())
 
